8abc741910629eefb4218250edc0aac8e18c42d5
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-10-16 15:50:32 +0530

    initial commit

---
+++ mossweb/home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+# Create your views here.
+
+
+def home(request):
+
+
+    if request.method == "POST":
+
+        print(request.FILES)
+        myfiles = request.FILES.getlist('files')
+        
+        fs = FileSystemStorage()
+
+        
+        if fs.exists("Kunal"):
+            files = fs.listdir("Kunal")[1]
+
+            for f in files:
+                fs.delete(f"Kunal/{f}")
+
+        for file in myfiles:
+
+
+            filename = fs.save(f"Kunal/{file.name}", file)
+
+    return render(request,"home.html")
